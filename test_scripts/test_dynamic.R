8654322aea7ee3a6f8b1e18be542ec08086f4e8e
Natalia Zuniga-Garcia <31483718+nzunigag@users.noreply.github.com>
2017-12-12 18:56:33 -0600

    remove gcc from makevars

--- test_scripts/test_dynamic.R
+++ test_scripts/test_dynamic.R
@@ -8,34 +8,32 @@ x <- austin %>%
   data.matrix()
 
 mod <- ddpn_init(
-  nparticles = 500,
-  alpha = 2,
+  nparticles = 1000,
+  alpha = 3,
   lambda = c(-97.731970, 30.302445),
-  kappa = .001,
+  kappa = .0001,
   nu = 2,
-  Omega =  0.01 ^ 2 * diag(2),
-  rho = 0.7,
-  thinprob = 0.0001
+  Omega =  0.01 ^ 2 * diag(2)
 ) 
 
 mod_trained1 <- mod %>% 
-  ddpn_mix(x, epochs = 5) 
-
-mod_trained2 <- mod_trained1 %>% 
-  ddpn_mix(x)
-
-mod_trained3 <- mod_trained2 %>% 
-  ddpn_mix(x)
-
-mod_trained4 <- mod_trained3 %>% 
-  ddpn_mix(x)
-# mod_trained2 <- mod_trained %>% 
+  ddpn_mix(x, epochs = 1) 
+# 
+# mod_trained2 <- mod_trained1 %>% 
 #   ddpn_mix(x)
+# 
+# mod_trained3 <- mod_trained2 %>% 
+#   ddpn_mix(x)
+# 
+# mod_trained4 <- mod_trained3 %>% 
+#   ddpn_mix(x)
+# # mod_trained2 <- mod_trained %>% 
+# #   ddpn_mix(x)
 
-spatial_plot(mod_trained1, x, polygons = FALSE)
-spatial_plot(mod_trained2, x, polygons = FALSE)
-spatial_plot(mod_trained3, x, polygons = FALSE)
-spatial_plot(mod_trained4, x, polygons = TRUE)
+spatial_plot(mod_trained1, x, polygons = TRUE)
+# spatial_plot(mod_trained2, x, polygons = FALSE)
+# spatial_plot(mod_trained3, x, polygons = FALSE)
+# spatial_plot(mod_trained4, x, polygons = TRUE)
 
 # mod_trained %>% 
 #   ddpn_mix(x) 
